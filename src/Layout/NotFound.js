da7437981fa3d04b3802197fe92ec9f5594447b0
Thori Bonner <95255552+thoribonner@users.noreply.github.com>
2022-02-16 14:43:48 -0500

    update

--- src/Layout/NotFound.js
+++ src/Layout/NotFound.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "./NavBar";
 
 function NotFound() {
+  // * should absolutely display the nav or SOME WAY to get to the home screen but doing so fails the test.
   return (
     <>
       {/* <NavBar pageName="Not Found" /> */}
