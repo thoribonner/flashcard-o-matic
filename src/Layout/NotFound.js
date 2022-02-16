540621d54017bcf4465ae901ede7fab2a70811ce
Thori Bonner <95255552+thoribonner@users.noreply.github.com>
2022-02-16 16:20:08 -0500

    updated NotFound

--- src/Layout/NotFound.js
+++ src/Layout/NotFound.js
@@ -5,8 +5,9 @@ function NotFound() {
   // * should absolutely display the nav or SOME WAY to get to the home screen but doing so fails the test.
   return (
     <>
-      {/* <NavBar pageName="Not Found" /> */}
+      <NavBar pageName="Not Found" />
       <h2>Not Found</h2>
+      <p>Yikes. There's nothing here. Maybe it's time to take a break, get some fresh air, drink some water, strecth your body a little bit. Before you go... I just want to say that you're doing great, and I'm really proud of you. 💛</p>
     </>
   );
 }
