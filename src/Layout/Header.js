d887b7fc9344786734b65fd463bcf324c0cc4ee8
Thori Bonner <95255552+thoribonner@users.noreply.github.com>
2022-03-01 23:58:50 -0500

    commit

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -4,9 +4,7 @@ function Header() {
   return (
     <header className="jumbotron bg-dark">
       <div className="container text-white">
-        
         <h1 className="display-4">Flashcard-o-matic</h1>
-
         <p className="lead">Discover The Flashcard Difference.</p>
       </div>
     </header>
