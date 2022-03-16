3af2f287ea97909dfcd3bb8eeb9a71954afcfdd6
Thori Bonner <95255552+thoribonner@users.noreply.github.com>
2022-03-16 14:38:50 -0400

    typo

--- src/Layout/NotFound.js
+++ src/Layout/NotFound.js
@@ -7,7 +7,7 @@ function NotFound() {
       <h2>Not Found</h2>
       <p>
         Yikes. There's nothing here. Maybe it's time to take a break, get some
-        fresh air, drink some water, strecth your body a little bit. Before you
+        fresh air, drink some water, stretch your body a little bit. Before you
         go... I just want to say that you're doing great, and I'm really proud
         of you. 💛
       </p>
